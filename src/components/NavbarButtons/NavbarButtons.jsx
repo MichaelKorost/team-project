ad8717e9004b6b2b79e9e5a8f0a7188a21fda030
MD <wkinfo@walla.com>
2022-11-17 22:19:23 +0200

    removing debug lines

--- src/components/NavbarButtons/NavbarButtons.jsx
+++ src/components/NavbarButtons/NavbarButtons.jsx
@@ -2,9 +2,7 @@ import {Link} from "react-router-dom";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
-import HomeIcon from '@mui/icons-material/Home';
 
 
 function NavbarButtons({setMenuState}) {
@@ -19,8 +17,7 @@ function NavbarButtons({setMenuState}) {
     },
     {
       text: "Homepage",
-      to: "/",
-      icon: HomeIcon
+      to: "/"
     },
     {
       text: "Profile",
@@ -32,111 +29,24 @@ function NavbarButtons({setMenuState}) {
     }
   ];
   return (
-    <List sx={{bgcolor: "background.paper"}} onClick={() => setMenuState(false)}>
-            <ListItemButton>
-          <ListItemIcon>
-            <HomeIcon />
-          </ListItemIcon>
-          <ListItemText primary="Chelsea Otakan" />
+    <List onClick={() => setMenuState(false)}>
+      <ListItem disablePadding sx={{backgroundColor: "#ccc", color: "#666"}}>
+        <ListItemButton disablePadding>
+          <ListItemText primary={"Close"} />
         </ListItemButton>
+      </ListItem>
       {
-        links.map(({text, to, icon}, i) => {
-          const Icon = icon;
+        links.map(({text, to}, i) => {
           return (
             <ListItem disablePadding key={i}>
-              <ListItemButton component={Link} to={to}>
-                {
-                  icon && <ListItemIcon>
-                    <Icon />
-                  </ListItemIcon>
-                }
+              <ListItemButton disablePadding component={Link} to={to}>
                 <ListItemText primary={text} />
               </ListItemButton>
             </ListItem>
           );
         })
       }
-      {/* <ListItem disablePadding>
-        <ListItemButton component={Link} to="/admin">
-          <ListItemText primary="Admin" />
-        </ListItemButton>
-      </ListItem>
-      <ListItem disablePadding>
-        <ListItemButton component={Link} to="/donors">
-          <ListItemText primary="Donors" />
-        </ListItemButton>
-      </ListItem>
-      <ListItem disablePadding>
-        <ListItemButton component={Link} to="/">
-          <ListItemText primary="Homepage" />
-        </ListItemButton>
-      </ListItem>
-      <ListItem disablePadding>
-        <ListItemButton component={Link} to="/profile">
-          <ListItemText primary="Profile" />
-        </ListItemButton>
-      </ListItem>
-      <ListItem disablePadding>
-        <ListItemButton component={Link} to="/logout">
-          <ListItemText primary="LogOut" />
-        </ListItemButton>
-      </ListItem> */}
     </List>
   );
 }
-
 export default NavbarButtons;
-
-
-
-
-// import {Link, useParams} from "react-router-dom";
-// import ButtonGroup from "@mui/material/ButtonGroup";
-// import Button from "@mui/material/Button";
-// import "./NavbarButtons.css";
-
-// function NavbarButtons() {
-
-//   // temporary consts START, get it using context
-//   const userIsLoggedIn = true;
-//   const userIsAdmin = true;
-//   const userIsMedicalCenterManager = true;
-//   // temporary consts END
-
-//   const params = useParams();
-//   const path = params && params.urlPath;
-//   const isOnUserPage = path && /^profile(?:\/|\?|#|$)/.test(path);
-//   const isOnDonorsPage = path && /^donors(?:\/|\?|#|$)/.test(path);
-//   const isOnAdminPage = path && /^admin(?:\/|\?|#|$)/.test(path);
-
-//   return (
-//       userIsLoggedIn &&
-//       <ButtonGroup variant="contained">
-//         {
-//           userIsAdmin && !isOnAdminPage && <Link to="/admin">
-//             <Button>Admin</Button>
-//           </Link>
-//         }
-//         {
-//           (userIsMedicalCenterManager || userIsAdmin) && !isOnDonorsPage && <Link to="/donors">
-//             <Button>Find Donors</Button>
-//           </Link>
-//         }
-//         {
-//           path && <Link to="/">
-//             <Button>Homepage</Button>
-//           </Link>
-//         }
-//         {
-//           !isOnUserPage && <Link to="/profile">
-//             <Button>Profile</Button>
-//           </Link>
-//         }
-//         <Link to="/logout">
-//           <Button>LogOut</Button>
-//         </Link>
-//       </ButtonGroup>
-//   );
-// }
-
-// export default NavbarButtons;
