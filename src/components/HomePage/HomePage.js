a5e9c9bca5dae167ab64031259fc9a3498af5511
Michael <michaelkorostilev@gmail.com>
2022-11-20 23:34:32 +0200

    feat[homepage] added homepage UI

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,26 @@
+import Navbar from "../Navbar/Navbar";
+import "./HomePage.css";
+
 const HomePage = () => {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <div className="homepage-page">
+        <div className="about">
+          <h1 className="about__title">About us</h1>
+          <p className="about__desc">
+            <span className="about__desc-bold">Blood Buddy</span> is an App that
+            makes donating blood effortless. <br /> Pick a date and we'll handle
+            the rest
+          </p>
+        </div>
+
+        <div className="button-container">
+          <button className="donate-button">I want to donate!</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
