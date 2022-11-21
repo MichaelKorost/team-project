a98d5ef2a4cbe6d6f8e881f5cc2ab6510b38c0e1
Michael <michaelkorostilev@gmail.com>
2022-11-21 11:41:25 +0200

    finished left pane

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -1,13 +1,45 @@
 import "./ProfilePage.css";
 import { Button, Typography } from "@mui/material";
 import Navbar from "../Navbar/Navbar";
+import { useState } from "react";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
 
 const ProfilePage = () => {
+  const [file, setFile] = useState("");
   return (
     <>
       <Navbar />
       <div className="profile-page-page">
-        <section className="left-pane"></section>
+        <section className="left-pane">
+          <div className="profile-image-container">
+            <img
+              className="profile__image"
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              }
+              alt="profile picture"
+            />
+            <Button
+              variant="contained"
+              component="label"
+              className="profile__image-button"
+            >
+              <CameraAltIcon />
+              Upload
+              <input hidden accept="image/*" multiple type="file" />
+            </Button>
+          </div>
+          <Button
+            disabled={false}
+            variant="contained"
+            className="profile__appointment-button"
+            style={{ margin: "auto" }}
+          >
+            View appointment details
+          </Button>
+        </section>
         <section className="right-pane"></section>
       </div>
     </>
