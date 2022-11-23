f712403ab392cfd79c3d519d7baae594dfedcc1a
Michael <michaelkorostilev@gmail.com>
2022-11-23 11:35:10 +0200

    added calendar and timer

--- src/components/DonatePage/DonatePage.js
+++ src/components/DonatePage/DonatePage.js
@@ -1,12 +1,55 @@
 import "./DonatePage.css";
 import ResponsiveAppBar from "../Navbar/Navbar";
+import Calendar from "../Calendar/Calender";
+import { useEffect, useState } from "react";
+import TimeInput from "../TimeInput/TimeInput";
+import { Button } from "@mui/material";
 
 const DonatePage = () => {
+  const [selectedDate, SetSelectedDate] = useState("");
+  const [dateString, setDateString] = useState("");
+  const [time, setTime] = useState(null);
+  const [timeString, setTimeString] = useState("");
+  const [isDateValid, setIsDateValid] = useState(false);
+
+  const calendarSelectedDate = (date) => {
+    SetSelectedDate(date);
+
+    const year = date?.getFullYear() || "";
+    const month = date?.toLocaleString("en-US", { month: "numeric" }) || "";
+    const day = date?.toDateString().slice(0, 3) || "";
+    const dayNum = date?.toLocaleString("en-US", { day: "2-digit" }) || "";
+    setDateString(
+      `an appointment was set on ${day}, ${dayNum}/${month}/${year}`
+    );
+  };
+
+  const timeSelect = (timeData) => {
+    setTime(timeData);
+    const timeString = timeData.toLocaleString().slice(12, 17);
+    setTimeString(timeString);
+  };
+
+  useEffect(() => {
+    setIsDateValid(dateString !== "" && timeString !== "");
+  }, [dateString, timeString]);
+
   return (
     <>
       <ResponsiveAppBar />
       <div className="donate-page-page">
-        <section className="donate-left-pane"></section>
+        <section className="donate-left-pane">
+          <h1 className="donate__title">Available dates at *hospital name*.</h1>
+          <Calendar onCalenderSelect={calendarSelectedDate} />
+          <TimeInput onTimeSelect={timeSelect} />
+          <Button
+            disabled={!isDateValid}
+            variant="text"
+            className="donate__button"
+          >
+            Set appointment
+          </Button>
+        </section>
         <section className="donate-right-pane"></section>
       </div>
     </>
---
+++ src/components/TimeInput/TimeInput.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./TimeInput.css";
+import dayjs from "dayjs";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+
+export default function TimeInput({ onTimeSelect }) {
+  const [value, setValue] = useState(dayjs().set("hour", 24).set("minute", 0));
+
+  const changeTimeHandler = (newValue) => {
+    onTimeSelect(newValue.$d);
+    setValue(newValue.$d);
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Stack spacing={3}>
+        <TimePicker
+          renderInput={(params) => <TextField {...params} />}
+          value={value}
+          label="Pick a time"
+          onChange={changeTimeHandler}
+          minTime={dayjs().set("hour", 8)}
+          maxTime={dayjs().set("hour", 12)}
+          minutesStep={10}
+          //   shouldDisableTime={(timeValue, clockType) => {
+          //     if (clockType === "minutes" && timeValue % 10) {
+          //       return true;
+          //     }
+
+          //     return false;
+          //   }}
+        />
+      </Stack>
+    </LocalizationProvider>
+  );
+}
