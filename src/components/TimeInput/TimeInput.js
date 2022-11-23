9f66880a513a5f2b1ec97f7bdc32f48e4b684e5a
Michael <michaelkorostilev@gmail.com>
2022-11-23 22:15:23 +0200

    fix[donate-page]time input cannot be cleared

--- src/components/TimeInput/TimeInput.js
+++ src/components/TimeInput/TimeInput.js
@@ -9,6 +9,7 @@ import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 
 export default function TimeInput({ onTimeSelect }) {
   const [value, setValue] = useState(dayjs().set("hour", 24).set("minute", 0));
+  const [open, setOpen] = useState(false);
 
   const changeTimeHandler = (newValue) => {
     onTimeSelect(newValue.$d);
@@ -19,13 +20,24 @@ export default function TimeInput({ onTimeSelect }) {
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Stack spacing={3}>
         <TimePicker
-          renderInput={(params) => <TextField {...params} />}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              onFocus={(e) => {
+                e.target.blur();
+                setOpen(true);
+              }}
+            />
+          )}
           value={value}
           label="Pick a time"
           onChange={changeTimeHandler}
           minTime={dayjs().set("hour", 8)}
           maxTime={dayjs().set("hour", 12)}
           minutesStep={10}
+          onOpen={() => setOpen(true)}
+          onClose={() => setOpen(false)}
+          open={open}
           //   shouldDisableTime={(timeValue, clockType) => {
           //     if (clockType === "minutes" && timeValue % 10) {
           //       return true;
