3c177e5324110ebc66ec0301981b5596f15007a8
Michael <michaelkorostilev@gmail.com>
2022-11-24 16:24:12 +0200

    feat[donate detail modal]added modal

--- src/App.js
+++ src/App.js
@@ -13,10 +13,9 @@ function App() {
       {/* <Navbar /> */}
       {/* <LoginForm /> */}
       {/* <HomePage /> */}
-      <ProfilePage />
-      {/* <DonatePage /> */}
+      {/* <ProfilePage /> */}
+      <DonatePage />
       {/* <AppointmentModal /> */}
-      {/* <DonateDetailsModal /> */}
     </div>
   );
 }
