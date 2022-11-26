e7c3af055ae607e0c0a629d3f65f5b6630a12216
Michael <michaelkorostilev@gmail.com>
2022-11-26 14:06:32 +0200

    uncommented loading page to get app to work

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,8 +1,8 @@
-import { Link } from 'react-router-dom';
-import Navbar from '../Navbar/Navbar';
-import './HomePage.css';
+import { Link } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import "./HomePage.css";
 // import { AuthContext } from '../../AuthContext';
-// import LoadingPage from '../LoadingPage/LoadingPage';
+import LoadingPage from "../LoadingPage/LoadingPage";
 // import { useContext } from 'react';
 
 const HomePage = () => {
@@ -12,19 +12,19 @@ const HomePage = () => {
   return (
     <>
       <Navbar />
-      <div className='homepage-page'>
-        <div className='about'>
-          <h1 className='about__title'>About us</h1>
-          <p className='about__desc'>
-            <span className='about__desc-bold'>Blood Buddy</span> is an App that
+      <div className="homepage-page">
+        <div className="about">
+          <h1 className="about__title">About us</h1>
+          <p className="about__desc">
+            <span className="about__desc-bold">Blood Buddy</span> is an App that
             makes donating blood effortless. <br /> Pick a date and we'll handle
             the rest 💞
           </p>
         </div>
 
-        <div className='button-container'>
-          <Link to='/donate'>
-            <button className='donate-button'>I want to donate!</button>
+        <div className="button-container">
+          <Link to="/donate">
+            <button className="donate-button">I want to donate!</button>
           </Link>
         </div>
       </div>
