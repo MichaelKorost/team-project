fd99a6312a49ae0e82a5880e18d8a56693b2f7b0
Noam Rivlin <noam.rivlin15@gmail.com>
2022-11-29 01:31:52 +0200

    feat[profileForm]added location input and validation, changed phone input fought,died, reborned and now css looks different for no reason

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { ThemeProvider, createTheme } from "@mui/material";
-import { AuthProvider } from "./AuthContext";
-import { AppointmentProvider } from "./contexts/AppointmentContext";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ThemeProvider, createTheme } from '@mui/material';
+import { AuthProvider } from './AuthContext';
+import { AppointmentProvider } from './contexts/AppointmentContext';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#ff4756",
+      main: '#ff4756',
     },
     success: {
-      main: "#2ed573",
+      main: '#2ed573',
     },
   },
 });
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
@@ -26,7 +26,7 @@ root.render(
         <ThemeProvider theme={theme}>
           <Router>
             <Routes>
-              <Route path="*" element={<App />}></Route>
+              <Route path='*' element={<App />}></Route>
             </Routes>
           </Router>
         </ThemeProvider>
